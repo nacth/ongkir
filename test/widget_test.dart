d9b685779ac830c3a4aed6bdb687f70aa28b48ee
Abdurrasyid Muhasibi <abdurrasyid.muhasibi.20@gmail.com>
2021-08-30 20:54:20 +0700

    basic http

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,17 +1,11 @@
 import 'package:http/http.dart' as http;
-import 'dart:convert';
-
-import 'package:ongkir/app/data/models/user_model.dart';
 
 void main() async {
-  Uri url = Uri.parse("https://reqres.in/api/users/10");
-  final response = await http.get(url);
-  final user =
-      UserModel.fromJson(json.decode(response.body) as Map<String, dynamic>);
+  Uri url = Uri.parse("https://reqres.in/api/users");
+  final response = await http.post(
+    url,
+    body: {"name": "morpheus", "job": "leader"},
+  );
 
-  final data = user.data;
-  print("${data?.firstName} ${user.data?.lastName}");
-
-  final myJson = userModelToJson(user);
-  print(myJson);
+  print(response.body);
 }
